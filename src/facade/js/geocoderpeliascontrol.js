f750f98dbb80d5af6c22ff96eae17d4240dbb758
Emilio Pardo <e.pardo.perez@gmail.com>
2022-02-16 13:28:44 +0100

    se añade comprobación de la existencia de adendum.cdau

--- src/facade/js/geocoderpeliascontrol.js
+++ src/facade/js/geocoderpeliascontrol.js
@@ -215,7 +215,7 @@ export default class GeocoderpeliasControl extends M.Control {
           } else {
             htmlParseElement += '<div class="result autocomplete" data-feature="' + element.properties.id + '"><i class="result-icon g-cartografia-pin"></i>' + element.properties.label + '</div>';
           }
-        }else {
+        } else {
           htmlParseElement += '<div class="result autocomplete" data-feature="' + element.properties.id + '"><i class="result-icon g-cartografia-pin"></i>' + element.properties.label + '</div>';
         }
       }
@@ -226,6 +226,7 @@ export default class GeocoderpeliasControl extends M.Control {
   }
 
   createFeature(data) {
+    console.log(data)
     let feature = new M.Feature();
     feature.setId(data.properties.id)
     if (data.properties.layer == 'street') {
@@ -245,104 +246,124 @@ export default class GeocoderpeliasControl extends M.Control {
       //   feature.setAttribute('competencia', data.properties.addendum.cdau.competencia);
       // }
     } else if (data.properties.layer == 'address') {
-      feature.setAttribute('tipo_portal', data.properties.addendum.cdau.tipo_portal_pk);
-      feature.setAttribute('vía', data.properties.street);
-      feature.setAttribute('portal', data.properties.housenumber);
-      // feature.setAttribute('etiqueta', data.properties.label);
-      // feature.setAttribute('nombre', data.properties.name);
-      // if (data.properties.neighbourhood) {
-      //   feature.setAttribute('barrio', data.properties.neighbourhood);
-      // }
-      // if (data.properties.borough) {
-      //   feature.setAttribute('distrito', data.properties.borough);
-      // }
-      feature.setAttribute('código_postal', data.properties.postalcode);
-      // if (data.properties.addendum.cdau.seccion_censal) {
-      //   feature.setAttribute('seccion_censal', data.properties.addendum.cdau.seccion_censal);
-      // }
-      feature.setAttribute('núcleo', data.properties.locality);
-      // feature.setAttribute('código_núcleo', data.properties.locality_a);
-      feature.setAttribute('municipio', data.properties.localadmin);
-      // feature.setAttribute('código_municipio', data.properties.localadmin_a);
-      // feature.setAttribute('county', data.properties.county),
-      // feature.setAttribute('macrocounty', data.properties.macrocounty),
-      feature.setAttribute('provincia', data.properties.region);
-      // feature.setAttribute('comunidad_autonoma', data.properties.macroregion);
-      // feature.setAttribute('pais', data.properties.country);
-      if (data.properties.addendum.cdau.refcatparc) {
-        feature.setAttribute('referencia_catastral_parcela', '<a href="https://www1.sedecatastro.gob.es/CYCBienInmueble/OVCListaBienes.aspx?del&muni&rc1=' + data.properties.addendum.cdau.refcatparc.substring(0, 7) + '&rc2=' + data.properties.addendum.cdau.refcatparc.substring(7, 14) + '" target="_blank">' + data.properties.addendum.cdau.refcatparc + '</a>')
-      }
-      if (data.properties.addendum.cdau.nom_tipo_a) {
-        feature.setAttribute('tipo_agrupación', data.properties.addendum.cdau.nom_tipo_a);
-      }
-      if (data.properties.addendum.cdau.nom_agrup) {
-        feature.setAttribute('nombre_agrupación', data.properties.addendum.cdau.nom_agrup);
-      }
-      if (data.properties.addendum.cdau.txt_app) {
-        feature.setAttribute('txt_app', data.properties.addendum.cdau.txt_app);
+      if (data.properties.hasOwnProperty('addendum') && data.properties.addendum.hasOwnProperty('cdau')) {
+        if (data.properties.addendum.cdau.tipo_portal_pk) {
+          feature.setAttribute('tipo_portal', data.properties.addendum.cdau.tipo_portal_pk);
+        }
+        feature.setAttribute('vía', data.properties.street);
+        feature.setAttribute('portal', data.properties.housenumber);
+        // feature.setAttribute('etiqueta', data.properties.label);
+        // feature.setAttribute('nombre', data.properties.name);
+        // if (data.properties.neighbourhood) {
+        //   feature.setAttribute('barrio', data.properties.neighbourhood);
+        // }
+        // if (data.properties.borough) {
+        //   feature.setAttribute('distrito', data.properties.borough);
+        // }
+        feature.setAttribute('código_postal', data.properties.postalcode);
+        // if (data.properties.addendum.cdau.seccion_censal) {
+        //   feature.setAttribute('seccion_censal', data.properties.addendum.cdau.seccion_censal);
+        // }
+        feature.setAttribute('núcleo', data.properties.locality);
+        // feature.setAttribute('código_núcleo', data.properties.locality_a);
+        feature.setAttribute('municipio', data.properties.localadmin);
+        // feature.setAttribute('código_municipio', data.properties.localadmin_a);
+        // feature.setAttribute('county', data.properties.county),
+        // feature.setAttribute('macrocounty', data.properties.macrocounty),
+        feature.setAttribute('provincia', data.properties.region);
+        // feature.setAttribute('comunidad_autonoma', data.properties.macroregion);
+        // feature.setAttribute('pais', data.properties.country);
+        if (data.properties.addendum.cdau.refcatparc) {
+          feature.setAttribute('referencia_catastral_parcela', '<a href="https://www1.sedecatastro.gob.es/CYCBienInmueble/OVCListaBienes.aspx?del&muni&rc1=' + data.properties.addendum.cdau.refcatparc.substring(0, 7) + '&rc2=' + data.properties.addendum.cdau.refcatparc.substring(7, 14) + '" target="_blank">' + data.properties.addendum.cdau.refcatparc + '</a>')
+        }
+        if (data.properties.addendum.cdau.nom_tipo_a) {
+          feature.setAttribute('tipo_agrupación', data.properties.addendum.cdau.nom_tipo_a);
+        }
+        if (data.properties.addendum.cdau.nom_agrup) {
+          feature.setAttribute('nombre_agrupación', data.properties.addendum.cdau.nom_agrup);
+        }
+        if (data.properties.addendum.cdau.txt_app) {
+          feature.setAttribute('txt_app', data.properties.addendum.cdau.txt_app);
+        }
+      } else {
+        feature.setAttribute('vía', data.properties.street);
+        feature.setAttribute('portal', data.properties.housenumber);
+        feature.setAttribute('código_postal', data.properties.postalcode);
+        feature.setAttribute('núcleo', data.properties.locality);
+        feature.setAttribute('provincia', data.properties.region);
       }
     } else if (data.properties.layer == 'venue') {
       feature.setAttribute('nombre', data.properties.name);
-      if (data.properties.addendum.cdau.categoria) {
-        feature.setAttribute('categoria', data.properties.addendum.cdau.categoria);
-      }
-      if (data.properties.addendum.cdau.tipo) {
-        feature.setAttribute('tipo', data.properties.addendum.cdau.tipo);
-      }
-      if (data.properties.addendum.cdau.tipo_ctr) {
-        feature.setAttribute('tipo_centro', data.properties.addendum.cdau.tipo_ctr);
-      }
-      if (data.properties.addendum.cdau.consejeria) {
-        feature.setAttribute('consejeria', data.properties.addendum.cdau.consejeria);
-      }
-      if (data.properties.addendum.cdau.nivel1) {
-        feature.setAttribute('nivel1', data.properties.addendum.cdau.nivel1);
-      }
-      if (data.properties.addendum.cdau.nivel2) {
-        feature.setAttribute('nivel2', data.properties.addendum.cdau.nivel2);
-      }
-      if (data.properties.addendum.cdau.dependencia) {
-        feature.setAttribute('dependencia', data.properties.addendum.cdau.dependencia);
-      }
-      if (data.properties.addendum.cdau.titularidad) {
-        feature.setAttribute('titularidad', data.properties.addendum.cdau.titularidad);
-      }
-      if (data.properties.addendum.cdau.tipo_portal_pk) {
-        feature.setAttribute('tipo_portal', data.properties.addendum.cdau.tipo_portal_pk);
-      }
-      feature.setAttribute('vía', data.properties.street);
-      feature.setAttribute('portal', data.properties.housenumber);
-      // feature.setAttribute('etiqueta', data.properties.label);
-      // if (data.properties.neighbourhood) {
-      //   feature.setAttribute('barrio', data.properties.neighbourhood);
-      // }
-      // if (data.properties.borough) {
-      //   feature.setAttribute('distrito', data.properties.borough);
-      // }
-      feature.setAttribute('código_postal', data.properties.postalcode);
-      // if (data.properties.addendum.cdau.seccion_censal) {
-      //   feature.setAttribute('seccion_censal', data.properties.addendum.cdau.seccion_censal);
-      // }
-      feature.setAttribute('núcleo', data.properties.locality);
-      // feature.setAttribute('código_núcleo', data.properties.locality_a);
-      feature.setAttribute('municipio', data.properties.localadmin);
-      // feature.setAttribute('código_municipio', data.properties.localadmin_a);
-      // feature.setAttribute('county', data.properties.county),
-      // feature.setAttribute('macrocounty', data.properties.macrocounty),
-      feature.setAttribute('provincia', data.properties.region);
-      // feature.setAttribute('comunidad_autonoma', data.properties.macroregion);
-      // feature.setAttribute('pais', data.properties.country);
-      if (data.properties.addendum.cdau.refcatparc) {
-        feature.setAttribute('referencia_catastral_parcela', '<a href="https://www1.sedecatastro.gob.es/CYCBienInmueble/OVCListaBienes.aspx?del&muni&rc1=' + data.properties.addendum.cdau.refcatparc.substring(0, 7) + '&rc2=' + data.properties.addendum.cdau.refcatparc.substring(7, 14) + '" target="_blank">' + data.properties.addendum.cdau.refcatparc + '</a>')
-      }
-      if (data.properties.addendum.cdau.nom_tipo_a) {
-        feature.setAttribute('tipo_agrupación', data.properties.addendum.cdau.nom_tipo_a);
-      }
-      if (data.properties.addendum.cdau.nom_agrup) {
-        feature.setAttribute('nombre_agrupación', data.properties.addendum.cdau.nom_agrup);
-      }
-      if (data.properties.addendum.cdau.txt_app) {
-        feature.setAttribute('txt_app', data.properties.addendum.cdau.txt_app);
+      if (data.properties.hasOwnProperty('addendum') && data.properties.addendum.hasOwnProperty('cdau')) {
+        if (data.properties.addendum.cdau.categoria) {
+          feature.setAttribute('categoria', data.properties.addendum.cdau.categoria);
+        }
+        if (data.properties.addendum.cdau.tipo) {
+          feature.setAttribute('tipo', data.properties.addendum.cdau.tipo);
+        }
+        if (data.properties.addendum.cdau.tipo_ctr) {
+          feature.setAttribute('tipo_centro', data.properties.addendum.cdau.tipo_ctr);
+        }
+        if (data.properties.addendum.cdau.consejeria) {
+          feature.setAttribute('consejeria', data.properties.addendum.cdau.consejeria);
+        }
+        if (data.properties.addendum.cdau.nivel1) {
+          feature.setAttribute('nivel1', data.properties.addendum.cdau.nivel1);
+        }
+        if (data.properties.addendum.cdau.nivel2) {
+          feature.setAttribute('nivel2', data.properties.addendum.cdau.nivel2);
+        }
+        if (data.properties.addendum.cdau.dependencia) {
+          feature.setAttribute('dependencia', data.properties.addendum.cdau.dependencia);
+        }
+        if (data.properties.addendum.cdau.titularidad) {
+          feature.setAttribute('titularidad', data.properties.addendum.cdau.titularidad);
+        }
+        if (data.properties.addendum.cdau.tipo_portal_pk) {
+          feature.setAttribute('tipo_portal', data.properties.addendum.cdau.tipo_portal_pk);
+        }
+        feature.setAttribute('vía', data.properties.street);
+        feature.setAttribute('portal', data.properties.housenumber);
+        // feature.setAttribute('etiqueta', data.properties.label);
+        // if (data.properties.neighbourhood) {
+        //   feature.setAttribute('barrio', data.properties.neighbourhood);
+        // }
+        // if (data.properties.borough) {
+        //   feature.setAttribute('distrito', data.properties.borough);
+        // }
+        feature.setAttribute('código_postal', data.properties.postalcode);
+        // if (data.properties.addendum.cdau.seccion_censal) {
+        //   feature.setAttribute('seccion_censal', data.properties.addendum.cdau.seccion_censal);
+        // }
+        feature.setAttribute('núcleo', data.properties.locality);
+        // feature.setAttribute('código_núcleo', data.properties.locality_a);
+        feature.setAttribute('municipio', data.properties.localadmin);
+        // feature.setAttribute('código_municipio', data.properties.localadmin_a);
+        // feature.setAttribute('county', data.properties.county),
+        // feature.setAttribute('macrocounty', data.properties.macrocounty),
+        feature.setAttribute('provincia', data.properties.region);
+        // feature.setAttribute('comunidad_autonoma', data.properties.macroregion);
+        // feature.setAttribute('pais', data.properties.country);
+        if (data.properties.addendum.cdau.refcatparc) {
+          feature.setAttribute('referencia_catastral_parcela', '<a href="https://www1.sedecatastro.gob.es/CYCBienInmueble/OVCListaBienes.aspx?del&muni&rc1=' + data.properties.addendum.cdau.refcatparc.substring(0, 7) + '&rc2=' + data.properties.addendum.cdau.refcatparc.substring(7, 14) + '" target="_blank">' + data.properties.addendum.cdau.refcatparc + '</a>')
+        }
+        if (data.properties.addendum.cdau.nom_tipo_a) {
+          feature.setAttribute('tipo_agrupación', data.properties.addendum.cdau.nom_tipo_a);
+        }
+        if (data.properties.addendum.cdau.nom_agrup) {
+          feature.setAttribute('nombre_agrupación', data.properties.addendum.cdau.nom_agrup);
+        }
+        if (data.properties.addendum.cdau.txt_app) {
+          feature.setAttribute('txt_app', data.properties.addendum.cdau.txt_app);
+        }
+      }else{
+        feature.setAttribute('vía', data.properties.street);
+        feature.setAttribute('portal', data.properties.housenumber);
+        feature.setAttribute('código_postal', data.properties.postalcode);
+        feature.setAttribute('núcleo', data.properties.locality);
+        feature.setAttribute('municipio', data.properties.localadmin);
+        feature.setAttribute('provincia', data.properties.region);
+
       }
     }
     feature.setGeometry(data.geometry)
